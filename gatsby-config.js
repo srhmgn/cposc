5992892928f05d4ac7e477877ed62712897d2456
Sarah Mogin <mogin@work.co>
2018-11-25 16:13:51 -0500

    Wire up contentful

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,19 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Gatsby - Contentful - Netlify | CPOSC',
   },
   plugins: [
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `f2ttuvk85ak3`,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
     'gatsby-plugin-react-helmet',
     {
       resolve: `gatsby-source-filesystem`,
